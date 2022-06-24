ba54e53c57b6a66753fd5a60c3e99658ff55866a
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2022-06-23 23:03:30 -0300

    calc altura e nós de uma abb

--- Estrutura de Dados 2/Árvore Busca Binária/abb.h
+++ Estrutura de Dados 2/Árvore Busca Binária/abb.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct ABB
 {
@@ -8,7 +9,10 @@ typedef struct ABB
     struct ABB *dir; // ponteiro subárvore direita
 } ABB;
 
+int ABB_Altura(ABB *A);
+int ABB_N(ABB *A);
 ABB* ABB_Criar (int chave, ABB* esq, ABB* dir); // também retorna o endereço de um novo nó
 void ABB_Inserir (ABB** A, int chave); // vamos trocar o valor que o ponteiro está apontando, por isso precisamos do endereço do ponteiro do nó da árvore, ou seja, ABB**
 ABB* ABB_Buscar(ABB* A, int chave); // retorna o endereço do elemento da árvore
 void AB_Destruir(ABB **A);
+void ABB_ImprimirEmOrdem(ABB *A);
