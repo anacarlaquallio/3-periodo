80af09a71afa5660d889757f94e1d7399b882471
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2022-06-23 23:03:47 -0300

    Revert "calc altura e nós de uma abb"
    This reverts commit ba54e53c57b6a66753fd5a60c3e99658ff55866a.

--- Estrutura de Dados 2/Árvore Busca Binária/abb.c
+++ Estrutura de Dados 2/Árvore Busca Binária/abb.c
@@ -1,44 +1,5 @@
 #include "abb.h"
 
-int ABB_Altura(ABB *A) // Conta altura da árvore
-{
-    if (A == NULL)
-        return 0;
-    int dir = ABB_Altura(A->dir);
-    int esq = ABB_Altura(A->esq);
-    if (dir > esq)
-        return dir + 1;
-    else
-        return esq + 1;
-
-    // Solução do professor
-    /*
-        if (A = NULL)
-            return 0;
-        int alt_esq = ABB_Altura(A->esq);
-        int alt_dir = ABB_Altura(A->dir);
-        if (alt_dir>alt_esq) return alt_dir+1;
-        else return alt_esq +1;
-    */
-}
-
-int ABB_N(ABB *A) // Conta nós da árvore
-{
-    // Minha solução:
-    if (A == NULL)
-        return 0;
-    int dir = ABB_N(A->dir);
-    int esq = ABB_N(A->esq);
-    return dir + esq + 1; // 1 pq conta o nó
-
-    /*
-    // Solução do professor
-    if (A == NULL)
-        return 0;
-    return 1 + ABB_N(A->esq) + ABB_N(A->dir);
-    */
-}
-
 ABB *ABB_Criar(int chave, ABB *esq, ABB *dir)
 {
     ABB *novo = malloc(sizeof(ABB)); // cria um novo nó e aloca espaço pra ele
@@ -57,25 +18,10 @@ void ABB_Inserir(ABB **A, int chave)
     }
     // A só é o endereço do ponteiro. *A é o ponteiro em si, que contém o valor
 
-    // Não insere repetido
-    if (chave == (*A)->chave)
-    {
-        return;
-    }
-    else if (chave < (*A)->chave)
-    {
-        ABB_Inserir(&(*A)->esq, chave);
-    }
+    if (chave <= (*A)->chave)
+        ABB_Inserir(&(*A)->esq, chave); // pega o endereço do ponteiro que a gente vai seguir, está passando a aresta
     else
         ABB_Inserir(&(*A)->dir, chave);
-
-    // Insere repetido
-    /*
-        if (chave <= (*A)->chave)
-            ABB_Inserir(&(*A)->esq, chave); // pega o endereço do ponteiro que a gente vai seguir, está passando a aresta
-        else
-            ABB_Inserir(&(*A)->dir, chave);
-    */
 }
 
 // é pré-ordem
@@ -100,13 +46,4 @@ void AB_Destruir(ABB **A)
     AB_Destruir(&(*A)->esq);
     AB_Destruir(&(*A)->dir);
     free(*A);
-}
-
-void ABB_ImprimirEmOrdem(ABB *A)
-{
-    if (A == NULL)
-        return;
-    ABB_ImprimirEmOrdem(A->esq);
-    printf("%d, ", A->chave);
-    ABB_ImprimirEmOrdem(A->dir);
 }
