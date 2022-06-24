c456a36981716231b5c6de8ec2b64112bfd28e3c
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2022-06-23 23:04:24 -0300

    Revert "Revert "calc altura e nós de uma abb""
    This reverts commit 80af09a71afa5660d889757f94e1d7399b882471.

--- Estrutura de Dados 2/Árvore Busca Binária/abb.c
+++ Estrutura de Dados 2/Árvore Busca Binária/abb.c
@@ -1,5 +1,44 @@
 #include "abb.h"
 
+int ABB_Altura(ABB *A) // Conta altura da árvore
+{
+    if (A == NULL)
+        return 0;
+    int dir = ABB_Altura(A->dir);
+    int esq = ABB_Altura(A->esq);
+    if (dir > esq)
+        return dir + 1;
+    else
+        return esq + 1;
+
+    // Solução do professor
+    /*
+        if (A = NULL)
+            return 0;
+        int alt_esq = ABB_Altura(A->esq);
+        int alt_dir = ABB_Altura(A->dir);
+        if (alt_dir>alt_esq) return alt_dir+1;
+        else return alt_esq +1;
+    */
+}
+
+int ABB_N(ABB *A) // Conta nós da árvore
+{
+    // Minha solução:
+    if (A == NULL)
+        return 0;
+    int dir = ABB_N(A->dir);
+    int esq = ABB_N(A->esq);
+    return dir + esq + 1; // 1 pq conta o nó
+
+    /*
+    // Solução do professor
+    if (A == NULL)
+        return 0;
+    return 1 + ABB_N(A->esq) + ABB_N(A->dir);
+    */
+}
+
 ABB *ABB_Criar(int chave, ABB *esq, ABB *dir)
 {
     ABB *novo = malloc(sizeof(ABB)); // cria um novo nó e aloca espaço pra ele
@@ -18,10 +57,25 @@ void ABB_Inserir(ABB **A, int chave)
     }
     // A só é o endereço do ponteiro. *A é o ponteiro em si, que contém o valor
 
-    if (chave <= (*A)->chave)
-        ABB_Inserir(&(*A)->esq, chave); // pega o endereço do ponteiro que a gente vai seguir, está passando a aresta
+    // Não insere repetido
+    if (chave == (*A)->chave)
+    {
+        return;
+    }
+    else if (chave < (*A)->chave)
+    {
+        ABB_Inserir(&(*A)->esq, chave);
+    }
     else
         ABB_Inserir(&(*A)->dir, chave);
+
+    // Insere repetido
+    /*
+        if (chave <= (*A)->chave)
+            ABB_Inserir(&(*A)->esq, chave); // pega o endereço do ponteiro que a gente vai seguir, está passando a aresta
+        else
+            ABB_Inserir(&(*A)->dir, chave);
+    */
 }
 
 // é pré-ordem
@@ -46,4 +100,13 @@ void AB_Destruir(ABB **A)
     AB_Destruir(&(*A)->esq);
     AB_Destruir(&(*A)->dir);
     free(*A);
+}
+
+void ABB_ImprimirEmOrdem(ABB *A)
+{
+    if (A == NULL)
+        return;
+    ABB_ImprimirEmOrdem(A->esq);
+    printf("%d, ", A->chave);
+    ABB_ImprimirEmOrdem(A->dir);
 }
--- Estrutura de Dados 2/Árvore Busca Binária/main.c
+++ Estrutura de Dados 2/Árvore Busca Binária/main.c
@@ -1,23 +1,36 @@
 #include "abb.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+int *gerar_vetor_aleatorio(int n, int seed, int max)
+{
+    int *v = malloc(sizeof(int) * n);
+    srand(seed);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = rand() % max;
+    }
+    return v;
+}
 
 int main()
 {
     ABB *r = NULL;
 
-    ABB_Inserir(&r, 5);
-    ABB_Inserir(&r, 4);
-    ABB_Inserir(&r, 20);
-    ABB_Inserir(&r, 2);
-    ABB_Inserir(&r, 6);
-    ABB_Inserir(&r, 1);
-    ABB_Inserir(&r, 3);
+    int v[] = {5, 2, 8, 1, 6};
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        ABB_Inserir(&r, v[i]);
+    }
 
-    printf("%p\n", ABB_Buscar(r, 1));
-    printf("%p\n", ABB_Buscar(r, 7));
-    printf("%p\n", ABB_Buscar(r, 2));
-    printf("%p\n", ABB_Buscar(r, 20));
-    printf("%p\n", ABB_Buscar(r, 8));
+    // int *v = gerar_vetor_aleatorio (n, 1, 10*n)
+    // int n = atoi(argv[1]);
+    // v[i] = rand & max
+
+    ABB_ImprimirEmOrdem(r);
+    printf("\nEsta arvore tem %d nos\n", ABB_N(r));
+    printf("Esta arvore tem %d de altura\n", ABB_Altura(r));
 
     return 0;
 }
